b6c9fa5ac3133234765682ed7ecf47d3cfc3650e
BrunaMassuchini <brunaccelestino@gmail.com>
2023-01-13 20:35:38 -0300

    feat: start project

---
+++ src/main/java/br/com/ada/AdaTask/AdaTaskApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.AdaTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaTaskApplication.class, args);
+	}
+
+}
