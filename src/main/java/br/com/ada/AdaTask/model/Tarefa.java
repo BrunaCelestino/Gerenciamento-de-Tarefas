88815f7307670014ede292142798a0dab0db5b22
Maria Laura <laura.alves@gocase.com.br>
2023-01-13 21:07:18 -0300

    Add Tarefa class

--- src/main/java/br/com/ada/AdaTask/model/Tarefa.java
+++ src/main/java/br/com/ada/AdaTask/model/Tarefa.java
@@ -1,4 +1,32 @@
 package br.com.ada.AdaTask.model;
 
+import br.com.ada.AdaTask.model.enums.Prioridade;
+import br.com.ada.AdaTask.model.enums.Status;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
 public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String nome;
+    private String descricao;
+    private Status status;
+    private LocalDateTime dataDeCriacao;
+    private LocalDateTime dataDeVencimento;
+    private Prioridade prioridade;
+    private Usuario responsavel;
+    private String projeto;
+    private String etiquetas;
+
+
 }
